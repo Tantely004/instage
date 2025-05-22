994a1e587b6fef40305c118a4f4e4efc2c1a90ac
Tantely <tandrianaridera@gmail.com>
2025-05-22 15:53:34 +0300

    Redirection functional
    Sauvegarde (sans gestion des routages illicites)

--- front-stage/src/App.jsx
+++ front-stage/src/App.jsx
@@ -13,28 +13,31 @@ import DashboardSupervisor from './pages/supervisor/Dashboard'
 import LayoutAdmin from './pages/admin/Layout'
 import DashboardAdmin from './pages/admin/Dashboard'
 
+// Middleware d’authentification
+import Auth from './middleware/Auth'
+
 function App() {
-      const [loading, setLoading] = useState(false)
-      const [isDarkMode, setIsDarkMode] = useState(() => {
-      return localStorage.getItem('theme') === 'dark'
+  const [loading, setLoading] = useState(false)
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem('theme') === 'dark'
   })
   const location = useLocation()
 
   useEffect(() => {
     if (isDarkMode) {
-        document.documentElement.classList.add('dark')
-        import('primereact/resources/themes/lara-dark-indigo/theme.css').then(() => {
-            const lightTheme = document.querySelector('link[href*="lara-light-indigo"]')
-            if (lightTheme) lightTheme.remove()
-        })
+      document.documentElement.classList.add('dark')
+      import('primereact/resources/themes/lara-dark-indigo/theme.css').then(() => {
+        const lightTheme = document.querySelector('link[href*="lara-light-indigo"]')
+        if (lightTheme) lightTheme.remove()
+      })
       localStorage.setItem('theme', 'dark')
     } else {
-        document.documentElement.classList.remove('dark')
-        import('primereact/resources/themes/lara-light-indigo/theme.css').then(() => {
-            const darkTheme = document.querySelector('link[href*="lara-dark-indigo"]')
-            if (darkTheme) darkTheme.remove()
-        })
-        localStorage.setItem('theme', 'light')
+      document.documentElement.classList.remove('dark')
+      import('primereact/resources/themes/lara-light-indigo/theme.css').then(() => {
+        const darkTheme = document.querySelector('link[href*="lara-dark-indigo"]')
+        if (darkTheme) darkTheme.remove()
+      })
+      localStorage.setItem('theme', 'light')
     }
   }, [isDarkMode])
 
@@ -46,8 +49,8 @@ function App() {
     window.addEventListener('load', handleComplete)
 
     return () => {
-        window.removeEventListener('beforeunload', handleStart)
-        window.removeEventListener('load', handleComplete)
+      window.removeEventListener('beforeunload', handleStart)
+      window.removeEventListener('load', handleComplete)
     }
   }, [])
 
@@ -55,24 +58,33 @@ function App() {
     <>
       <AnimatePresence mode="wait">
         <div>
-            <Routes key={location.pathname} location={location}>
-                <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/intern" element={<LayoutIntern />}>
-                  <Route index path="dashboard" element={<Dashboard />} />
-              </Route>
+          <Routes key={location.pathname} location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
 
-              <Route 
-                  path="/supervisor" 
-                  element={<LayoutSupervisor setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode} />}
-              >
-                  <Route index path="dashboard" element={<DashboardSupervisor />} />
+            {/* Routes Intern sécurisées */}
+            <Route element={<Auth allowedRoles={['intern']} />}>
+              <Route path="/intern" element={<LayoutIntern />}>
+                <Route index path="dashboard" element={<Dashboard />} />
               </Route>
+            </Route>
 
+            {/* Routes Supervisor/Instructors sécurisées */}
+            <Route element={<Auth allowedRoles={['supervisor', 'instructor']} />}>
+              <Route path="/supervisor" element={
+                <LayoutSupervisor isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+              }>
+                <Route index path="dashboard" element={<DashboardSupervisor />} />
+              </Route>
+            </Route>
+
+            {/* Routes Admin sécurisées */}
+            <Route element={<Auth allowedRoles={['admin', 'administrator']} />}>
               <Route path="/admin" element={<LayoutAdmin />}>
-                  <Route index path="dashboard" element={<DashboardAdmin />} />
+                <Route index path="dashboard" element={<DashboardAdmin />} />
               </Route>
-            </Routes>
+            </Route>
+          </Routes>
         </div>
       </AnimatePresence>
     </>
