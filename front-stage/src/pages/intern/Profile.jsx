bc1734b5a2a3649f5eb8563bf373baefdd6d8d70
jerry <jerry@mg-consulting-it-act.com>
2025-05-28 12:05:51 +0300

    feat: implement profile page for intern

--- front-stage/src/pages/intern/Profile.jsx
+++ front-stage/src/pages/intern/Profile.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react"
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion"
+import { InputText } from "primereact/inputtext"
+import { Password } from "primereact/password"
+import { PanelMenu } from 'primereact/panelmenu'
+import { Chip } from 'primereact/chip'
 
 import imgIntern from "../../assets/images/fake/intern2.png"
 
 const ProfileIntern = () => {
+    const sessionItems = [
+        { 
+            label: 'Connexions' ,
+            items: [
+                {
+                    label: "Voir vos activités récentes",
+                    icon: 'pi pi-clock'
+                },
+                {
+                    label: "Appareils connectés",
+                    icon: 'pi pi-mobile'
+                },
+            ]
+        }
+    ]
+
     const pageVariants = {
         initial: { opacity: 0, y: -10 },
         in: { opacity: 1, y: 0 },
@@ -13,6 +34,28 @@ const ProfileIntern = () => {
     const pageTransition = {
         duration: 0.5,
     }
+
+    const [personalInfo, setPersonalInfo] = useState({
+        lastname: "ANDRIANARIDERA",
+        firstname: "Tantely Ny Aina",
+        email: "taxandrian@gmail.com",
+        contact: "+261 32 76 098 32",
+        etablishment: "ESMIA",
+        domain: "Informatique",
+        level: "Licence 3",
+    })
+
+    const [ passwordUser, setPasswordUser ] = useState({
+        current: "taxandrian",
+        new: '',
+        confirm: '',
+    })
+
+    const [ personalLink, setPersonalLink ] = useState({
+        linkedin: 'https://www.linkedin.com/in/janedoe',
+        portfolio: 'https://janedoe-portfolio.com',
+        github: 'https://github.com/janedoe'
+    })
 
     return (
         <motion.div
@@ -31,6 +74,9 @@ const ProfileIntern = () => {
                     src={imgIntern}
                     className="w-40 h-40 rounded-full absolute -bottom-16 left-1/8 border-6 border-white"
                 />
+                <p className="absolute -bottom-8 left-80 text-gray-600 text-sm">
+                    Dernière connexion: il y a 16 heures
+                </p>
             </div>
 
             <div className="mt-20 px-36 flex justify-between">
@@ -47,14 +93,29 @@ const ProfileIntern = () => {
                     <p className="text-gray-600">
                         <i className="pi pi-graduation-cap mt-2 mr-3"/>
                         Université ESMIA - Mahamasina
+
                     </p>
                 </div>
 
-                <div>
+                <div className="flex flex-col justify-between ml-auto">
                     <i 
-                        className="pi pi-ellipsis-v cursor-pointer hover:text-indigo-400"
+                        className="pi pi-ellipsis-v cursor-pointer text-end hover:text-indigo-400"
                         title="Options"
                     />
+                    <div class="text-end">
+                        <span className="font-medium text-sm">
+                            CV : 
+                        </span>
+                        <Chip 
+                            label="CV_Tantely_Ny_Aina"
+                            icon="pi pi-file"
+                            className="!ml-4 !text-xs !font-poppins"
+                        />
+                        <i 
+                            className="pi pi-pen-to-square ml-4 text-indigo-500 text-xs"
+                            title="Éditer"
+                        />
+                    </div>
                 </div>
             </div>
 
@@ -66,8 +127,78 @@ const ProfileIntern = () => {
                         Informations personnelles
                     </h3>
 
-                    <div className="bg-white shadow rounded p-6">
-                        
+                    <div className="mt-4 flex flex-col space-y-5 bg-gray-50 shadow-lg rounded-lg p-6">
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                <i className="pi pi-user mr-2 text-indigo-400"/> Nom
+                            </label>
+                            <InputText
+                                value={personalInfo.lastname}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Prénoms
+                            </label>
+                            <InputText
+                                value={personalInfo.firstname}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Adresse e-mail
+                            </label>
+                            <InputText
+                                type="email"
+                                value={personalInfo.email}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Contact
+                            </label>
+                            <InputText
+                                value={personalInfo.contact}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Établissement d'origine
+                            </label>
+                            <InputText
+                                value={personalInfo.etablishment}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Domaine
+                            </label>
+                            <InputText
+                                value={personalInfo.domain}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                Niveau
+                            </label>
+                            <InputText
+                                value={personalInfo.level}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
                     </div>
                 </section>
 
@@ -75,6 +206,84 @@ const ProfileIntern = () => {
                     <h3 className="text-indigo-500 font-semibold text-xl">
                         Mot de passe et sécurité
                     </h3>
+
+                    <div className="mt-4 flex flex-col space-y-5 bg-gray-50 shadow-lg rounded-lg p-6">
+                        <div className="flex justify-between items-center">                    
+                            <p className="text-sm">
+                                Votre mot de passe est fourni par l'administrateur. Vous pouvez personnaliser votre mot de passe à partir de cet alternative 
+                            </p>
+                            <i className="pi pi-info-circle text-xs cursor-pointer hover:text-indigo-400"/>
+                        </div>
+
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                <i className="pi pi-lock text-indigo-400 mr-2"/> Votre mot de passe actuel
+                            </label>
+                            <Password
+                                value={passwordUser.current}
+                                pt={{
+                                    input: "!font-poppins !h-11"
+                                }}
+                            />
+                            <p className="mt-6 font-medium text-indigo-500 text-sm">
+                                <i className="pi pi-shield mr-3"/>
+                                Changer mot de passe ?
+                            </p>
+                        </div>
+
+                        <div className="mt-2">
+                            <PanelMenu 
+                                model={sessionItems}
+                                pt={{
+                                    header: "!font-poppins !font-semibold",
+                                    headerContent: '!border-none',
+                                    panel: '!font-poppins',
+                                }}
+                            />
+                        </div>
+                    </div>
+
+                    <h3 className="mt-12 text-indigo-500 font-semibold text-xl">
+                        Liens externes
+                    </h3>
+                            
+                    <div className="mt-4 flex flex-col space-y-5 bg-gray-50 shadow-lg rounded-lg p-6">
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                <i className="pi pi-linkedin text-indigo-500 mr-3" />
+                                Profil Linkedin
+                            </label>
+                            <InputText
+                                value={personalLink.linkedin}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                <i className="pi pi-globe text-indigo-500 mr-3" />
+                                Portfolio
+                            </label>
+                            <InputText
+                                value={personalLink.portfolio}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+
+                        <div className="flex flex-col space-y-2">
+                            <label className="text-gray-500">
+                                <i className="pi pi-github text-indigo-500 mr-3" />
+                                Profil Github
+                            </label>
+                            <InputText
+                                value={personalLink.github}
+                                size="small"
+                                className="!font-poppins"
+                            />
+                        </div>
+                    </div>
                 </section>
             </form>
 
