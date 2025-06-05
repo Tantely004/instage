dc40ed1f58b803530d044a4664490b27e1db700b
jerry <jerry@mg-consulting-it-act.com>
2025-06-05 21:02:52 +0300

    feat: implement layout

---
+++ front-stage/src/pages/supervisor/Planning/index.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'framer-motion'
+
+const CalendarSupervisor = () => {
+    const pageVariants = {
+        initial: { opacity: 0, y: -10 },
+        in: { opacity: 1, y: 0 },
+        out: { opacity: 0, y: -5 },
+    }
+
+    const pageTransition = {
+        duration: 0.5,
+    }
+
+    return (
+        <motion.div
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={pageVariants}
+            transition={pageTransition} 
+        >
+        </motion.div>
+    )
+}
+
+export default CalendarSupervisor
